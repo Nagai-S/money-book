dd73585a0c1f0dd8eed49875f7fca7bdbdcda14f
Nagai-S <63888620+Nagai-S@users.noreply.github.com>
2020-09-03 23:59:48 +0900

    migration file

--- db/migrate/20200902084153_change_datatype_pay_date_of_credits.rb
+++ db/migrate/20200902084153_change_datatype_pay_date_of_credits.rb
@@ -1,5 +1,8 @@
 class ChangeDatatypePayDateOfCredits < ActiveRecord::Migration[5.1]
-  def change
+  def up
     change_column :credits, :pay_date, 'integer'
   end
+
+  def down
+    change_column :credits, :pay_date, 'date'
 end
--- db/migrate/20200902084206_change_datatype_month_date_of_credits.rb
+++ db/migrate/20200902084206_change_datatype_month_date_of_credits.rb
@@ -1,5 +1,9 @@
 class ChangeDatatypeMonthDateOfCredits < ActiveRecord::Migration[5.1]
-  def change
+  def up
     change_column :credits, :month_date, 'integer'
+  end
+
+  def down
+    change_column :credits, :month_date, 'date'
   end
 end

05cdc92bf7d6a4d6c50aca1b54307dd417fd3d26
Nagai-S <63888620+Nagai-S@users.noreply.github.com>
2020-09-03 22:42:39 +0900

    change credit-new

--- app/models/credit.rb
+++ app/models/credit.rb
@@ -3,11 +3,15 @@ class Credit < ApplicationRecord
   validates :name, presence: true, uniqueness: { scope: :user }
   validates :account, presence: true
   validates :pay_date, presence: true,
-                       numericality: { only_integer: true},
-                       numericality: {greater_than_or_equal_to: 1},
-                       numericality: {less_than_or_equal_to: 31}
+                       numericality: {
+                         only_integer: true,
+                         greater_than_or_equal_to: 1,
+                         less_than_or_equal_to: 31
+                       }
   validates :month_date, presence: true,
-                       numericality: { only_integer: true},
-                       numericality: {greater_than_or_equal_to: 1},
-                       numericality: {less_than_or_equal_to: 31}
+                       numericality: {
+                         only_integer: true,
+                         greater_than_or_equal_to: 1,
+                         less_than_or_equal_to: 31
+                       }
 end

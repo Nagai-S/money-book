c41cc551bd88a5af2b6f017d73bbbcbc33a2f175
Nagai-S <63888620+Nagai-S@users.noreply.github.com>
2020-10-20 21:02:20 +0900

    search genre
    検索のジャンルの場所を変更した

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -92,24 +92,6 @@ $(function(){
       }
     });
 
-  $('#select_js1').change(function() {
-    // 選択されているvalue属性値を取り出す
-    var val = $('#select_js1').val();
-    if (val=="0") {
-      $('#all').css("display", "block");
-      $('#e').css("display", "none");
-      $('#i').css("display", "none");
-    }else if (val=="true") {
-      $('#i').css("display", "block");
-      $('#e').css("display", "none");
-      $('#all').css("display", "none");
-    }else{
-      $('#e').css("display", "block");
-      $('#i').css("display", "none");
-      $('#all').css("display", "none");
-    }
-  });
-
   $('#select_js3').change(function() {
     // 選択されているvalue属性値を取り出す
     var val = $('#select_js3').val();
@@ -130,12 +112,4 @@ $(function(){
     }
   });
 
-  // var select1=document.getElementsByName("[date1(2i)]");
-  // $('name="[date1(2i)]"').change(function() {
-  //   // 選択されているvalue属性値を取り出す
-  //   var val = $('name="[date1(2i)]"').val();
-  //   if (val=="2" || val=="4" || val=="6" || val=="9" || val=="11") {
-  //     select1.remove(30)
-  //   }
-  // });
 });
